d0e56d31b67d47e0d8e11f0f8d46451bf4f0c390
vladkop2025 <email@example.ru>
2025-02-07 17:58:35 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Модуль 4. Основы C# и CLR. Основные конструкции C#
+        }
+    }
+}
